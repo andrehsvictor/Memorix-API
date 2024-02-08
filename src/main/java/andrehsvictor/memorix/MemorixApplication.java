8f529110091740e0e8b5261a0d9dc31a17ce4664
André Victor <andrehsvictor@gmail.com>
2024-02-08 14:27:19 -0300

    first commit

---
+++ src/main/java/andrehsvictor/memorix/MemorixApplication.java
@@ -0,0 +1,13 @@
+package andrehsvictor.memorix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorixApplication.class, args);
+	}
+
+}
