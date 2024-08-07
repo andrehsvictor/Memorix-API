a0b5b0f27dde89ec6b7ce0fcbf05d4af0420231b
André Victor <andrehsvictor@gmail.com>
2024-08-07 17:21:03 -0300

    feat: Add SQL script to populate decks table

---
+++ src/main/resources/db/migration/V1.0.2__populate_decks_table.sql
@@ -0,0 +1,42 @@
+insert into decks (name, slug, description, user_id) values ('Universal dynamic ability', 'universal-dynamic-ability', 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.', 5);
+insert into decks (name, slug, description, user_id) values ('Inverse incremental matrix', 'inverse-incremental-matrix', null, 3);
+insert into decks (name, slug, description, user_id) values ('Upgradable neutral infrastructure', 'upgradable-neutral-infrastructure', null, 5);
+insert into decks (name, slug, description, user_id) values ('Fundamental modular local area network', 'fundamental-modular-local-area-network', null, 2);
+insert into decks (name, slug, description, user_id) values ('Re-engineered 24/7 array', 're-engineered-24/7-array', null, 2);
+insert into decks (name, slug, description, user_id) values ('Cross-group maximized portal', 'cross-group-maximized-portal', null, 1);
+insert into decks (name, slug, description, user_id) values ('Up-sized full-range archive', 'up-sized-full-range-archive', null, 2);
+insert into decks (name, slug, description, user_id) values ('Enhanced multimedia hardware', 'enhanced-multimedia-hardware', 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.
+
+In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.', 2);
+insert into decks (name, slug, description, user_id) values ('Adaptive interactive model', 'adaptive-interactive-model', null, 2);
+insert into decks (name, slug, description, user_id) values ('User-friendly reciprocal algorithm', 'user-friendly-reciprocal-algorithm', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.
+
+Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.
+
+Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.', 5);
+insert into decks (name, slug, description, user_id) values ('Function-based human-resource neural-net', 'function-based-human-resource-neural-net', null, 1);
+insert into decks (name, slug, description, user_id) values ('Optimized local adapter', 'optimized-local-adapter', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 4);
+insert into decks (name, slug, description, user_id) values ('Re-engineered maximized moderator', 're-engineered-maximized-moderator', null, 1);
+insert into decks (name, slug, description, user_id) values ('Multi-tiered responsive Graphical User Interface', 'multi-tiered-responsive-graphical-user-interface', null, 4);
+insert into decks (name, slug, description, user_id) values ('Organized incremental complexity', 'organized-incremental-complexity', 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
+
+Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.
+
+Phasellus in felis. Donec semper sapien a libero. Nam dui.', 5);
+insert into decks (name, slug, description, user_id) values ('Fully-configurable 24/7 budgetary management', 'fully-configurable-24/7-budgetary-management', 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.
+
+Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.
+
+Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.', 2);
+insert into decks (name, slug, description, user_id) values ('Persevering actuating capability', 'persevering-actuating-capability', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
+
+Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
+
+Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 4);
+insert into decks (name, slug, description, user_id) values ('Robust impactful product', 'robust-impactful-product', 'Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.
+
+Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.', 3);
+insert into decks (name, slug, description, user_id) values ('Compatible demand-driven contingency', 'compatible-demand-driven-contingency', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.
+
+Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.', 2);
+insert into decks (name, slug, description, user_id) values ('Networked well-modulated archive', 'networked-well-modulated-archive', null, 3);
