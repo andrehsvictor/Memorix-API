0eb8f7e48ae95c1b2721f0af029c97cce3cd09f1
André Victor <andrehsvictor@gmail.com>
2024-08-07 17:20:47 -0300

    feat: Add UserDetailsImpl class for handling user details

---
+++ src/main/java/andrehsvictor/memorix/security/UserDetailsImpl.java
@@ -0,0 +1,39 @@
+package andrehsvictor.memorix.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import andrehsvictor.memorix.entity.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    } 
+}
