9956efc3a32477e3aa162510df9376fd62f58926
André Victor <andrehsvictor@gmail.com>
2024-08-07 11:10:26 -0300

    chore: Add User entity with necessary fields and annotations

---
+++ src/main/java/andrehsvictor/memorix/entity/User.java
@@ -0,0 +1,57 @@
+package andrehsvictor.memorix.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@EqualsAndHashCode
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+    private String avatarUrl;
+
+    @Builder.Default
+    private boolean deleted = false;
+
+    @Builder.Default
+    private boolean enabled = false;
+
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Builder.Default
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public void delete() {
+        this.deleted = true;
+    }
+
+    public void enable() {
+        this.enabled = true;
+    }
+}
