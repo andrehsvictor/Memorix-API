55ed6745d9b5a033c2c9cc487f9b315a47134aff
André Victor <andrehsvictor@gmail.com>
2024-08-08 08:56:29 -0300

    chore: Update setup.sh script and .env.example
    This commit updates the setup.sh script to include copying the .env.example file and adds a message to remind users to fill in the .env file with their own values. These changes improve the setup process and provide clearer instructions for configuring the application.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 mkdir -p keys && cd keys
 openssl genpkey -algorithm RSA -out private.key -pkeyopt rsa_keygen_bits:2048
 openssl rsa -pubout -in private.key -out public.key
+
+cd ..
+cp .env.example .env
+
+echo "Please fill in the .env file with your own values"
