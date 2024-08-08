606e552f1f1bbfae61571a0e431c34a99b8951ac
André Victor <andrehsvictor@gmail.com>
2024-08-08 15:20:45 -0300

    refactor: Add validation annotations to AccessTokenService expiry field
    The `AccessTokenService` class has been updated to include a validation annotation for the `expiry` field. This change ensures that the access token expiry duration is provided and improves the reliability of the service.

--- src/main/java/andrehsvictor/memorix/service/AccessTokenService.java
+++ src/main/java/andrehsvictor/memorix/service/AccessTokenService.java
@@ -10,14 +10,18 @@ import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 
 @Service
+@Validated
 @RequiredArgsConstructor
 public class AccessTokenService {
 
     @Value("${memorix.jwt.access.token.expiry:15m}")
+    @NotNull(message = "Access token expiry must be provided")
     private Duration expiry = Duration.ofMinutes(15);
 
     private final JwtEncoder jwtEncoder;
