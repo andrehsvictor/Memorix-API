9d93330e3b2f753fa12439d51a98fb60b4599879
André Victor <andrehsvictor@gmail.com>
2024-08-08 07:36:12 -0300

    feat: Add AccessTokenService for generating access tokens
    This commit adds a new service called AccessTokenService to the application. The AccessTokenService is responsible for generating access tokens based on the provided authentication. It uses the JwtEncoder to encode the tokens with the necessary claims, including the subject, issued at time, and expiration time. This addition enhances the security and authentication capabilities of the application.

---
+++ src/main/java/andrehsvictor/memorix/service/AccessTokenService.java
@@ -0,0 +1,38 @@
+package andrehsvictor.memorix.service;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AccessTokenService {
+
+    @Value("${memorix.jwt.access.token.expiry:15m}")
+    private Duration expiry = Duration.ofMinutes(15);
+
+    private final JwtEncoder jwtEncoder;
+
+    public Jwt generate(Authentication authentication) {
+        String sub = authentication.getName();
+        Instant iat = Instant.now();
+        Instant exp = iat.plus(expiry);
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .subject(sub)
+                .issuedAt(iat)
+                .expiresAt(exp)
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims));
+    }
+}
