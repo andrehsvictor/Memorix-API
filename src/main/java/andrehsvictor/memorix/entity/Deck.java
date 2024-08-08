884119b848e6460d7fc814f7cd521c19dc814ff1
André Victor <andrehsvictor@gmail.com>
2024-08-08 16:39:56 -0300

    feat: Add Deck entity for managing decks
    This commit adds a new entity, Deck, responsible for managing decks. It includes properties for the deck's ID, name, slug, description, user, creation and update timestamps. This addition enhances the functionality of the application by providing a structured representation of deck data in the database.

---
+++ src/main/java/andrehsvictor/memorix/entity/Deck.java
@@ -0,0 +1,45 @@
+package andrehsvictor.memorix.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "decks")
+@EqualsAndHashCode(of = "id")
+public class Deck {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String slug;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime updatedAt;
+}
