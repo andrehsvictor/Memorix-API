bbaa80df2ac1896e90805c3ceeace6277ccaa25d
André Victor <andrehsvictor@gmail.com>
2024-08-08 15:21:09 -0300

    refactor: Update EmailService to use MimeMessageHelper for sending emails
    The `EmailService` class has been updated to use `MimeMessageHelper` for sending emails. This change improves the email sending functionality by providing support for HTML content and setting the sender's email address.

--- src/main/java/andrehsvictor/memorix/service/EmailService.java
+++ src/main/java/andrehsvictor/memorix/service/EmailService.java
@@ -1,23 +1,29 @@
 package andrehsvictor.memorix.service;
 
-import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import jakarta.mail.Message.RecipientType;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
-    private final MailSender mailSender;
+    private final JavaMailSender mailSender;
 
     public void send(String to, String subject, String body) {
-        ((JavaMailSender) mailSender).send(mimeMessage -> {
-            mimeMessage.setRecipients(RecipientType.TO, to);
-            mimeMessage.setSubject(subject);
-            mimeMessage.setText(body);
-        });
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            helper.setFrom("andre.sousa@dcx.ufpb.br");
+            mailSender.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
