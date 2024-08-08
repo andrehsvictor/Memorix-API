b2173a63fed5b1607dd0121f9de6795571142fef
André Victor <andrehsvictor@gmail.com>
2024-08-08 15:21:28 -0300

    refactor: Improve activation code handling in SignupService
    The `SignupService` class has been refactored to improve activation code handling. The `activate` method now validates the activation code, activates the user, and deletes the activation code. Additionally, the `resendActivationCode` method checks if the user is already activated and if the activation code is expired before creating and sending a new activation code. This refactor enhances the functionality and reliability of the SignupService.

--- src/main/java/andrehsvictor/memorix/service/SignupService.java
+++ src/main/java/andrehsvictor/memorix/service/SignupService.java
@@ -1,10 +1,14 @@
 package andrehsvictor.memorix.service;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import andrehsvictor.memorix.entity.ActivationCode;
 import andrehsvictor.memorix.entity.User;
+import andrehsvictor.memorix.exception.MemorixException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -24,5 +28,37 @@ public class SignupService {
         String subject = "Memorix - Activation Code";
         String body = "Your activation code is: " + activationCode.getCode();
         emailService.send(to, subject, body);
+    }
+
+    public void activate(String code) {
+        activationCodeService.validate(code);
+        ActivationCode activationCode = activationCodeService.findByCode(code);
+        User user = activationCode.getUser();
+        userService.activate(user);
+        activationCodeService.delete(activationCode);
+    }
+
+    public void resendActivationCode(String email) {
+        User user = userService.findByUsernameOrEmail(email);
+        checkUserAlreadyActivated(user);
+        checkActivationCodeNotExpired(user);
+        ActivationCode activationCode = activationCodeService.create(user);
+        String to = user.getEmail();
+        String subject = "Memorix - Activation Code";
+        String body = "Your activation code is: " + activationCode.getCode();
+        emailService.send(to, subject, body);
+    }
+
+    private void checkActivationCodeNotExpired(User user) {
+        ActivationCode activationCode = user.getActivationCode();
+        if (!activationCode.getExpiresAt().isBefore(LocalDateTime.now())) {
+            throw new MemorixException(HttpStatus.BAD_REQUEST, "Activation code is not expired yet. Please wait for the current activation code to expire.");
+        }
+    }
+
+    private void checkUserAlreadyActivated(User user) {
+        if (user.isEnabled()) {
+            throw new MemorixException(HttpStatus.BAD_REQUEST, "User is already activated.");
+        }
     }
 }
