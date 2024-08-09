2f0ae9b19e4d1df5bca011efe562b109e201e77d
André Victor <andrehsvictor@gmail.com>
2024-08-08 21:44:37 -0300

    feat: Refactor DeckController to use MappingJacksonValue for response serialization

--- src/main/java/andrehsvictor/memorix/controller/DeckController.java
+++ src/main/java/andrehsvictor/memorix/controller/DeckController.java
@@ -1,16 +1,14 @@
 package andrehsvictor.memorix.controller;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import andrehsvictor.memorix.dto.ResponseBody;
-import andrehsvictor.memorix.dto.response.DeckResponseDTO;
 import andrehsvictor.memorix.entity.Deck;
 import andrehsvictor.memorix.presenter.DeckPresenter;
 import andrehsvictor.memorix.service.DeckService;
@@ -25,14 +23,14 @@ public class DeckController {
     private final DeckPresenter deckPresenter;
 
     @GetMapping("/{slug}")
-    public ResponseBody<DeckResponseDTO> findBySlug(@PathVariable String slug,
-            @RequestParam(defaultValue = "name,slug,description,createdAt,updatedAt,totalCards,totalCardsToReview") String include) {
+    public MappingJacksonValue findBySlug(@PathVariable String slug, @RequestParam(defaultValue = "*") String include) {
         Deck deck = deckService.findBySlug(slug);
-        return deckPresenter.present(deck);
+        return deckPresenter.present(deck, include);
     }
 
     @GetMapping
-    public ResponseBody<List<DeckResponseDTO>> findAll(Pageable pageable) {
-        return deckPresenter.present(deckService.findAll(pageable));
+    public MappingJacksonValue findAll(Pageable pageable, @RequestParam(defaultValue = "*") String include) {
+        Page<Deck> decks = deckService.findAll(pageable);
+        return deckPresenter.present(decks, include);
     }
 }
