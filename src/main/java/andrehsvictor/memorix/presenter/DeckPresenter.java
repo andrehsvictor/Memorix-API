8278ae698b9f8309ba6882451d12ba06cd05b937
André Victor <andrehsvictor@gmail.com>
2024-08-08 21:45:25 -0300

    feat: Refactor DeckPresenter to use MappingJacksonValue for response serialization

--- src/main/java/andrehsvictor/memorix/presenter/DeckPresenter.java
+++ src/main/java/andrehsvictor/memorix/presenter/DeckPresenter.java
@@ -3,7 +3,11 @@ package andrehsvictor.memorix.presenter;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 import andrehsvictor.memorix.controller.DeckController;
 import andrehsvictor.memorix.dto.ResponseBody;
@@ -17,21 +21,40 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 @RequiredArgsConstructor
 public class DeckPresenter {
 
-    public ResponseBody<DeckResponseDTO> present(Deck deck) {
-        DeckResponseDTO responseDTO = new DeckResponseDTO(deck);
+    public MappingJacksonValue present(Deck deck, String fields) {
+        DeckResponseDTO responseDTO = convertToDTO(deck);
+        ResponseBody<DeckResponseDTO> responseBody = createResponseBody(responseDTO);
 
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(responseBody);
+        mappingJacksonValue.setFilters(createFilter(fields));
+        return mappingJacksonValue;
+    }
+
+    public MappingJacksonValue present(Page<Deck> decks, String fields) {
+        List<DeckResponseDTO> responseDTOs = decks.map(this::convertToDTO).getContent();
+
+        ResponseBody<List<DeckResponseDTO>> responseBody = createResponseBody(decks, responseDTOs);
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(responseBody);
+        mappingJacksonValue.setFilters(createFilter(fields));
+        return mappingJacksonValue;
+    }
+
+    private DeckResponseDTO convertToDTO(Deck deck) {
+        DeckResponseDTO responseDTO = new DeckResponseDTO(deck);
+        responseDTO.add(linkTo(methodOn(DeckController.class).findBySlug(deck.getSlug(), "*"))
+                .withSelfRel());
+        return responseDTO;
+    }
+
+    private ResponseBody<DeckResponseDTO> createResponseBody(DeckResponseDTO responseDTO) {
         return ResponseBody.<DeckResponseDTO>builder()
                 .data(responseDTO)
                 .build();
     }
 
-    public ResponseBody<List<DeckResponseDTO>> present(Page<Deck> decks) {
-        List<DeckResponseDTO> responseDTOs = decks.map((deck) -> {
-            DeckResponseDTO responseDTO = new DeckResponseDTO(deck);
-            responseDTO.add(linkTo(methodOn(DeckController.class).findBySlug(deck.getSlug(), null)).withSelfRel());
-            return responseDTO;
-        }).getContent();
-
+    private ResponseBody<List<DeckResponseDTO>> createResponseBody(Page<Deck> decks,
+            List<DeckResponseDTO> responseDTOs) {
         return ResponseBody.<List<DeckResponseDTO>>builder()
                 .data(responseDTOs)
                 .totalElements(decks.getTotalElements())
@@ -41,6 +64,13 @@ public class DeckPresenter {
                 .sort(decks.getSort().toString())
                 .hasNext(decks.hasNext())
                 .build();
+    }
+
+    private SimpleFilterProvider createFilter(String fields) {
+        SimpleBeanPropertyFilter filter = (fields == null || fields.equals("*"))
+                ? SimpleBeanPropertyFilter.serializeAll()
+                : SimpleBeanPropertyFilter.filterOutAllExcept(fields.split(","));
+        return new SimpleFilterProvider().addFilter("DeckResponseDTO", filter);
     }
 
 }
