8ce1d2dda4586c1a2c97616cea05aaff7f071a75
andrehsvictor <andrehsvictor@gmail.com>
2024-11-26 16:00:21 -0300

    feat: add JwtConfig for configuring JWT encoding and decoding with RSA keys

---
+++ src/main/java/andrehsvictor/memorix/config/JwtConfig.java
@@ -0,0 +1,48 @@
+package andrehsvictor.memorix.config;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.validation.annotation.Validated;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import jakarta.validation.constraints.NotNull;
+
+@Validated
+@Configuration
+public class JwtConfig {
+
+    @NotNull(message = "The public key must be provided.")
+    @Value("${memorix.security.jwt.access-token.public-key.location}")
+    private RSAPublicKey publicKey;
+
+    @NotNull(message = "The private key must be provided.")
+    @Value("${memorix.security.jwt.access-token.private-key.location}")
+    private RSAPrivateKey privateKey;
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
+        JWKSet jwkSet = new JWKSet(jwk);
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(jwkSet);
+        return new NimbusJwtEncoder(jwkSource);
+    }
+}
