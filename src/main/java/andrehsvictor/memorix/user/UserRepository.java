4d2dd276b24ad09ab2aab980bf6512f45f7becd6
andrehsvictor <andrehsvictor@gmail.com>
2024-11-26 09:11:51 -0300

    feat: enhance UserService and UserRepository with additional user retrieval and existence checks

--- src/main/java/andrehsvictor/memorix/user/UserRepository.java
+++ src/main/java/andrehsvictor/memorix/user/UserRepository.java
@@ -1,9 +1,16 @@
 package andrehsvictor.memorix.user;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 
 }
--- src/main/java/andrehsvictor/memorix/user/UserService.java
+++ src/main/java/andrehsvictor/memorix/user/UserService.java
@@ -1,11 +1,37 @@
 package andrehsvictor.memorix.user;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
+import andrehsvictor.memorix.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with username: " + username + "."));
+    }
+
+    public User findById(UUID id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with ID: " + id + "."));
+    }
 }
