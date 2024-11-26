1ec73755371aad28234bce88ea467fee5dc41b40
andrehsvictor <andrehsvictor@gmail.com>
2024-11-26 08:45:31 -0300

    feat: add UserRepository interface for user data access

---
+++ src/main/java/andrehsvictor/memorix/user/UserRepository.java
@@ -0,0 +1,9 @@
+package andrehsvictor.memorix.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
