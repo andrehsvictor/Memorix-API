462f9822c344e691157c54f1785a99b19269bd42
andrehsvictor <andrehsvictor@gmail.com>
2024-11-26 10:34:14 -0300

    feat: enhance GlobalExceptionHandler to provide detailed field error responses

--- src/main/java/andrehsvictor/memorix/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/andrehsvictor/memorix/exception/handler/GlobalExceptionHandler.java
@@ -1,14 +1,23 @@
 package andrehsvictor.memorix.exception.handler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import andrehsvictor.memorix.exception.ResourceAlreadyExistsException;
 import andrehsvictor.memorix.exception.ResourceNotFoundException;
 import andrehsvictor.memorix.exception.dto.ErrorDto;
+import andrehsvictor.memorix.exception.dto.FieldErrorDto;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -35,4 +44,19 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDto);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<FieldErrorDto> fieldErrorDto = new ArrayList<>();
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            FieldErrorDto error = FieldErrorDto.builder()
+                    .field(fieldError.getField())
+                    .message(fieldError.getDefaultMessage())
+                    .build();
+            fieldErrorDto.add(error);
+        }
+        ErrorDto<FieldErrorDto> errorDto = new ErrorDto<>();
+        errorDto.setErrors(fieldErrorDto);
+        return ResponseEntity.badRequest().body(errorDto);
+    }
 }
