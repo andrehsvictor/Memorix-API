bcb4d6286e242afe28f50b4d19aae9ffa14a3002
andrehsvictor <andrehsvictor@gmail.com>
2024-11-28 16:57:54 -0300

    feat: add RefreshToken entity for managing refresh tokens in the database

---
+++ src/main/java/andrehsvictor/memorix/token/refreshtoken/RefreshToken.java
@@ -0,0 +1,45 @@
+package andrehsvictor.memorix.token.refreshtoken;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import andrehsvictor.memorix.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = { "user" })
+@Table(name = "refresh_tokens")
+public class RefreshToken implements Serializable {
+
+    private static final long serialVersionUID = -4157557238683463036L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String token;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime expiresAt;
+
+}
