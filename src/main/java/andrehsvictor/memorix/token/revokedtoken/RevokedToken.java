592ebd3af2d80f51af56414bead850abeafa5fef
andrehsvictor <andrehsvictor@gmail.com>
2024-11-28 16:58:03 -0300

    feat: add RevokedToken entity for managing revoked tokens in the database

---
+++ src/main/java/andrehsvictor/memorix/token/revokedtoken/RevokedToken.java
@@ -0,0 +1,34 @@
+package andrehsvictor.memorix.token.revokedtoken;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "revoked_tokens")
+public class RevokedToken implements Serializable {
+
+    private static final long serialVersionUID = 7340811607801323734L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String token;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
