1ce39e4e6a32017de0bc20822096fc406db6b5b5
andrehsvictor <andrehsvictor@gmail.com>
2024-11-29 13:34:39 -0300

    feat: update access token expiry configuration to use 'expires-in' property and add validation

--- src/main/java/andrehsvictor/memorix/token/accesstoken/AccessTokenService.java
+++ src/main/java/andrehsvictor/memorix/token/accesstoken/AccessTokenService.java
@@ -5,22 +5,26 @@ import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import andrehsvictor.memorix.token.jwt.JwtService;
 import andrehsvictor.memorix.token.jwt.JwtType;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 
 @Service
+@Validated
 @RequiredArgsConstructor
 public class AccessTokenService {
 
     private final JwtService jwtService;
 
-    @Value("${memorix.security.jwt.access-token.expiry:PT15M}")
-    private Duration expiry = Duration.ofMinutes(15);
+    @NotNull(message = "The access token expires in must be set.")
+    @Value("${memorix.security.jwt.access-token.expires-in:15m}")
+    private Duration expiresIn = Duration.ofMinutes(15);
 
     public AccessToken issue(String subject) {
-        Jwt jwt = jwtService.issue(subject, JwtType.ACCESS, expiry);
+        Jwt jwt = jwtService.issue(subject, JwtType.ACCESS, expiresIn);
         return AccessToken.of(jwt);
     }
 }
