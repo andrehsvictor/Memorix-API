3902939c7b1cd3f4be96724b4cf9b495420643bb
andrehsvictor <andrehsvictor@gmail.com>
2024-11-29 15:11:31 -0300

    refactor: remove NotRevoked validation annotation and its validator

--- src/main/java/andrehsvictor/memorix/token/dto/RefreshTokenDto.java
+++ src/main/java/andrehsvictor/memorix/token/dto/RefreshTokenDto.java
@@ -1,6 +1,5 @@
 package andrehsvictor.memorix.token.dto;
 
-import andrehsvictor.memorix.token.validation.NotRevoked;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +8,6 @@ import lombok.Setter;
 @Setter
 public class RefreshTokenDto {
 
-    @NotRevoked
     @NotBlank(message = "The refresh token must be provided.")
     private String refreshToken;
 
