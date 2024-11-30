ad3241eac89ad4af97a9e59899f351f8476eff63
andrehsvictor <andrehsvictor@gmail.com>
2024-11-30 20:45:54 -0300

    feat: implement UserDetailsImpl class for user authentication

---
+++ src/main/java/andrehsvictor/memorix/security/UserDetailsImpl.java
@@ -0,0 +1,36 @@
+package andrehsvictor.memorix.security;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import andrehsvictor.memorix.user.User;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private static final long serialVersionUID = -3966817104016432200L;
+
+    private User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+}
