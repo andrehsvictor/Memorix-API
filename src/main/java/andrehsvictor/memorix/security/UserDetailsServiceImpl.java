e30f544f77ae5ee4309b83345c5352aa7ee5254b
andrehsvictor <andrehsvictor@gmail.com>
2024-11-30 20:45:46 -0300

    refactor: rename UserDetailsServiceImpl class to security package for better organization

--- src/main/java/andrehsvictor/memorix/security/UserDetailsServiceImpl.java
+++ src/main/java/andrehsvictor/memorix/security/UserDetailsServiceImpl.java
@@ -1,10 +1,11 @@
-package andrehsvictor.memorix.user;
+package andrehsvictor.memorix.security;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import andrehsvictor.memorix.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
