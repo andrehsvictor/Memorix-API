2be96ba9f991e869467c752f12bb1315266c28a8
andrehsvictor <andrehsvictor@gmail.com>
2024-12-01 13:41:06 -0300

    feat: integrate JwtFilter into SecurityConfig for enhanced JWT validation

--- src/main/java/andrehsvictor/memorix/config/SecurityConfig.java
+++ src/main/java/andrehsvictor/memorix/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package andrehsvictor.memorix.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,14 +14,19 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import andrehsvictor.memorix.security.JwtFilter;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
-    @Autowired
-    private JwtDecoder jwtDecoder;
+    private final JwtDecoder jwtDecoder;
+    private final JwtFilter jwtFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -32,6 +36,7 @@ public class SecurityConfig {
             authorize.requestMatchers(HttpMethod.POST, "/auth/token").permitAll();
             authorize.anyRequest().permitAll();
         });
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         http.oauth2ResourceServer((oauth2) -> oauth2.jwt((jwt) -> jwt.decoder(jwtDecoder)));
         return http.build();
     }
