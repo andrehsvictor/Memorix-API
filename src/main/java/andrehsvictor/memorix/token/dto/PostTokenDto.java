2638cad471c3787a56d0e97b140a58fc2e076ea9
andrehsvictor <andrehsvictor@gmail.com>
2024-12-03 17:03:39 -0300

    feat: add validation annotations for username and password in PostTokenDto

--- src/main/java/andrehsvictor/memorix/token/dto/PostTokenDto.java
+++ src/main/java/andrehsvictor/memorix/token/dto/PostTokenDto.java
@@ -1,5 +1,6 @@
 package andrehsvictor.memorix.token.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,10 @@ import lombok.Setter;
 @Setter
 public class PostTokenDto {
 
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @NotBlank(message = "Password is required")
     private String password;
 
 }
