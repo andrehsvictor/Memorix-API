2a052772d6c6c03122050742e7a358f06c4d0915
andrehsvictor <andrehsvictor@gmail.com>
2024-12-03 20:47:19 -0300

    feat: enhance UserMapper to include mapping for PostUserDto with default values

--- src/main/java/andrehsvictor/memorix/user/UserMapper.java
+++ src/main/java/andrehsvictor/memorix/user/UserMapper.java
@@ -1,12 +1,21 @@
 package andrehsvictor.memorix.user;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import andrehsvictor.memorix.user.dto.GetMeDto;
+import andrehsvictor.memorix.user.dto.PostUserDto;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     GetMeDto userToGetMeDto(User user);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "enabled", constant = "false")
+    @Mapping(target = "emailVerified", constant = "false")
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    User postUserDtoToUser(PostUserDto postUserDto);
 
 }
