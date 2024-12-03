660744d41bf387c8eb9d2dabc726e714615618b9
andrehsvictor <andrehsvictor@gmail.com>
2024-12-03 17:00:08 -0300

    feat: implement UserDetails and UserDetailsService for user authentication

---
+++ src/main/java/andrehsvictor/memorix/security/UserDetailsImpl.java
@@ -0,0 +1,42 @@
+package andrehsvictor.memorix.security;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import andrehsvictor.memorix.user.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails, CredentialsContainer {
+
+    private static final long serialVersionUID = -3966817104016432200L;
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public void eraseCredentials() {
+        user.setPassword(null);
+    }
+
+}
---
+++ src/main/java/andrehsvictor/memorix/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package andrehsvictor.memorix.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import andrehsvictor.memorix.user.User;
+import andrehsvictor.memorix.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username + "."));
+        return new UserDetailsImpl(user);
+    }
+
+}
