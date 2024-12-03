07df565d7d5cedf7f9a3f3035567828f5485667f
andrehsvictor <andrehsvictor@gmail.com>
2024-12-03 17:03:35 -0300

    fix: refine error message for user not found in UserDetailsServiceImpl

--- src/main/java/andrehsvictor/memorix/security/UserDetailsServiceImpl.java
+++ src/main/java/andrehsvictor/memorix/security/UserDetailsServiceImpl.java
@@ -18,7 +18,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsernameOrEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username + "."));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
         return new UserDetailsImpl(user);
     }
 
