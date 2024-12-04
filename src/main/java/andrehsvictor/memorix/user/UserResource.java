40ed482cb1152445c3a14e1020a5f37d920e1dbb
andrehsvictor <andrehsvictor@gmail.com>
2024-12-03 21:11:52 -0300

    feat: add user retrieval endpoints for searching and fetching by username

--- src/main/java/andrehsvictor/memorix/user/UserResource.java
+++ src/main/java/andrehsvictor/memorix/user/UserResource.java
@@ -2,14 +2,18 @@ package andrehsvictor.memorix.user;
 
 import java.net.URI;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import andrehsvictor.memorix.user.dto.GetMeDto;
+import andrehsvictor.memorix.user.dto.GetUserDto;
 import andrehsvictor.memorix.user.dto.PostUserDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -34,5 +38,24 @@ public class UserResource {
         GetMeDto getMeDto = userMapper.userToGetMeDto(user);
         URI location = URI.create("/v1/users/" + user.getUsername());
         return ResponseEntity.created(location).body(getMeDto);
+    }
+
+    @GetMapping("/v1/users")
+    public Page<GetUserDto> findAll(@Valid UserQuery userQuery, Pageable pageable) {
+        Page<User> users;
+        if (userQuery.getDisplayName() != null || userQuery.getUsername() != null) {
+            users = userService.searchByDisplayNameOrUsername(userQuery.getDisplayName(), userQuery.getUsername(),
+                    pageable);
+        } else {
+            users = userService.findAll(pageable);
+        }
+        return users.map(userMapper::userToGetUserDto);
+    }
+
+    @GetMapping("/v1/users/{username}")
+    public ResponseEntity<GetUserDto> findByUsername(@PathVariable String username) {
+        User user = userService.findByUsername(username);
+        GetUserDto getUserDto = userMapper.userToGetUserDto(user);
+        return ResponseEntity.ok(getUserDto);
     }
 }
