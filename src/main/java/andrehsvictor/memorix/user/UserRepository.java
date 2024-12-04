af05bbfc2255b90dff3054d3747e81936520d466
andrehsvictor <andrehsvictor@gmail.com>
2024-12-03 21:11:58 -0300

    feat: add pagination support for user retrieval by display name or username

--- src/main/java/andrehsvictor/memorix/user/UserRepository.java
+++ src/main/java/andrehsvictor/memorix/user/UserRepository.java
@@ -3,6 +3,8 @@ package andrehsvictor.memorix.user;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -14,5 +16,8 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+
+    Page<User> findAllByDisplayNameOrUsernameContainingIgnoreCase(String displayName, String username,
+            Pageable pageable);
 
 }
