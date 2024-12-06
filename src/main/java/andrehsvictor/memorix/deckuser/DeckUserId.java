41d1e222b7b4c642885c59a738550906a952b2bb
andrehsvictor <andrehsvictor@gmail.com>
2024-12-06 14:18:46 -0300

    feat: enhance DeckUserId class with Lombok annotations for improved code simplicity and readability

--- src/main/java/andrehsvictor/memorix/deckuser/DeckUserId.java
+++ src/main/java/andrehsvictor/memorix/deckuser/DeckUserId.java
@@ -3,6 +3,8 @@ package andrehsvictor.memorix.deckuser;
 import java.io.Serializable;
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,9 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(of = { "deckId", "userId" })
 public class DeckUserId implements Serializable {
 
