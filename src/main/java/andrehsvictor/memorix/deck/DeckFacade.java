6865bc48c96099230b086e8f82c8d9b8b5748b05
andrehsvictor <andrehsvictor@gmail.com>
2024-12-06 13:45:57 -0300

    feat: implement Deck management operations including create, update, delete, and user access management

--- src/main/java/andrehsvictor/memorix/deck/DeckFacade.java
+++ src/main/java/andrehsvictor/memorix/deck/DeckFacade.java
@@ -7,6 +7,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import andrehsvictor.memorix.deck.dto.GetDeckDto;
+import andrehsvictor.memorix.deck.dto.PostDeckDto;
+import andrehsvictor.memorix.deck.dto.PutDeckDto;
+import andrehsvictor.memorix.deckuser.DeckUser;
+import andrehsvictor.memorix.deckuser.DeckUserRole;
+import andrehsvictor.memorix.deckuser.DeckUserService;
+import andrehsvictor.memorix.exception.ForbiddenActionException;
+import andrehsvictor.memorix.user.User;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -14,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class DeckFacade {
 
     private final DeckService deckService;
+    private final DeckUserService deckUserService;
     private final DeckMapper deckMapper;
 
     public GetDeckDto findByIdAndUserIdOrVisibilityPublic(UUID id, UUID userId) {
@@ -39,6 +47,58 @@ public class DeckFacade {
     public Page<GetDeckDto> findAllByOwnerUsernameAndVisibilityPublic(String username, Pageable pageable) {
         Page<Deck> decks = deckService.findAllByOwnerUsernameAndVisibilityPublic(username, pageable);
         return decks.map(deckMapper::deckToGetDeckDto);
+    }
+
+    public GetDeckDto create(PostDeckDto postDeckDto, User user) {
+        Deck deck = deckMapper.postDeckDtoToDeck(postDeckDto);
+        deck.setOwner(user);
+        deck = deckService.save(deck);
+        DeckUser deckUser = of(user, deck, DeckUserRole.OWNER);
+        deckUserService.save(deckUser);
+        return deckMapper.deckToGetDeckDto(deck);
+    }
+
+    private DeckUser of(User user, Deck deck, DeckUserRole role) {
+        return DeckUser.builder()
+                .deck(deck)
+                .user(user)
+                .role(role)
+                .build();
+    }
+
+    public void deleteById(UUID id, User user) {
+        Deck deck = deckService.findByIdAndUserIdOrVisibilityPublic(id, user.getId());
+        if (!deck.getOwner().getId().equals(user.getId())) {
+            throw new ForbiddenActionException("You are not allowed to delete this deck");
+        }
+        deckService.deleteById(id);
+    }
+
+    public GetDeckDto update(UUID id, PutDeckDto putDeckDto, User user) {
+        Deck deck = deckService.findByIdAndUserIdOrVisibilityPublic(id, user.getId());
+        if (!deck.getOwner().getId().equals(user.getId())) {
+            throw new ForbiddenActionException("You are not allowed to update this deck");
+        }
+        deck = deckMapper.updateDeckFromPutDeckDto(putDeckDto, deck);
+        deck = deckService.save(deck);
+        return deckMapper.deckToGetDeckDto(deck);
+    }
+
+    public void add(UUID id, User user) {
+        Deck deck = deckService.findByIdAndUserIdOrVisibilityPublic(id, user.getId());
+        if (deckUserService.existsByDeckIdAndUserId(deck.getId(), user.getId())) {
+            throw new ForbiddenActionException("You already have access to this deck");
+        }
+        DeckUser deckUser = of(user, deck, DeckUserRole.USER);
+        deckUserService.save(deckUser);
+    }
+
+    public void remove(UUID id, User user) {
+        Deck deck = deckService.findByIdAndUserIdOrVisibilityPublic(id, user.getId());
+        if (!deckUserService.existsByDeckIdAndUserId(deck.getId(), user.getId())) {
+            throw new ForbiddenActionException("You do not have access to this deck");
+        }
+        deckUserService.deleteByDeckIdAndUserId(deck.getId(), user.getId());
     }
 
 }
--- src/main/java/andrehsvictor/memorix/deck/DeckResource.java
+++ src/main/java/andrehsvictor/memorix/deck/DeckResource.java
@@ -1,18 +1,27 @@
 package andrehsvictor.memorix.deck;
 
+import java.net.URI;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import andrehsvictor.memorix.deck.dto.GetDeckDto;
+import andrehsvictor.memorix.deck.dto.PostDeckDto;
+import andrehsvictor.memorix.deck.dto.PutDeckDto;
 import andrehsvictor.memorix.user.User;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -45,5 +54,37 @@ public class DeckResource {
     @GetMapping("/v1/users/me/decks")
     public Page<GetDeckDto> findAllMine(Pageable pageable, @AuthenticationPrincipal User user) {
         return deckFacade.findAllByUserId(user.getId(), pageable);
+    }
+
+    @PostMapping("/v1/decks")
+    public ResponseEntity<GetDeckDto> create(@Valid @RequestBody PostDeckDto postDeckDto,
+            @AuthenticationPrincipal User user) {
+        GetDeckDto deckDto = deckFacade.create(postDeckDto, user);
+        URI location = URI.create("/v1/decks/" + deckDto.getId());
+        return ResponseEntity.created(location).body(deckDto);
+    }
+
+    @PutMapping("/v1/decks/{id}")
+    public GetDeckDto update(@PathVariable String id, @Valid @RequestBody PutDeckDto putDeckDto,
+            @AuthenticationPrincipal User user) {
+        return deckFacade.update(UUID.fromString(id), putDeckDto, user);
+    }
+
+    @DeleteMapping("/v1/decks/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id, @AuthenticationPrincipal User user) {
+        deckFacade.deleteById(UUID.fromString(id), user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/v1/decks/{id}/add")
+    public ResponseEntity<Void> add(@PathVariable String id, @AuthenticationPrincipal User user) {
+        deckFacade.add(UUID.fromString(id), user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/v1/decks/{id}/remove")
+    public ResponseEntity<Void> remove(@PathVariable String id, @AuthenticationPrincipal User user) {
+        deckFacade.remove(UUID.fromString(id), user);
+        return ResponseEntity.noContent().build();
     }
 }
