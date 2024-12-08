7a1d2bd0309c1458e6f4259fd9033b599285511e
andrehsvictor <andrehsvictor@gmail.com>
2024-12-08 14:47:03 -0300

    refactor: enhance DeckRepository with additional query methods and delete functionality

--- src/main/java/andrehsvictor/memorix/deck/DeckRepository.java
+++ src/main/java/andrehsvictor/memorix/deck/DeckRepository.java
@@ -1,14 +1,29 @@
 package andrehsvictor.memorix.deck;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface DeckRepository extends JpaRepository<Deck, UUID> {
 
     Optional<Deck> findBySlugAndUserId(String slug, UUID userId);
 
     boolean existsBySlugAndUserId(String slug, UUID userId);
-    
+
+    void deleteBySlugAndUserId(String slug, UUID userId);
+
+    Page<Deck> findAllByUserId(UUID userId, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Deck d WHERE d.slug IN :slugs AND d.user.id = :userId")
+    void deleteAllWithSlugsAndUserId(Set<String> slugs, UUID userId);
+
 }
