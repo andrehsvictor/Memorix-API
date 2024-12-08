728eecd684d5fd06d3294babe413cd61f2f85cda
andrehsvictor <andrehsvictor@gmail.com>
2024-12-08 09:30:02 -0300

    feat: set default value for id column in users table to generate random UUID

--- src/main/resources/db/migration/V20241208083954__create_users_table.sql
+++ src/main/resources/db/migration/V20241208083954__create_users_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    id             UUID PRIMARY KEY,
+    id             UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     display_name   VARCHAR(255) NOT NULL,
     username       VARCHAR(255) NOT NULL,
     email          VARCHAR(255) NOT NULL,
