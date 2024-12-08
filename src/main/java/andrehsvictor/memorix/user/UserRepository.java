0369b14d86871d6d111291de679452d0becd6721
andrehsvictor <andrehsvictor@gmail.com>
2024-12-08 14:47:15 -0300

    refactor: remove unused pagination method from UserRepository

--- src/main/java/andrehsvictor/memorix/user/UserRepository.java
+++ src/main/java/andrehsvictor/memorix/user/UserRepository.java
@@ -3,8 +3,6 @@ package andrehsvictor.memorix.user;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
@@ -12,8 +10,5 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
-
-    Page<User> findAllByDisplayNameOrUsernameContainingIgnoreCase(String displayName, String username,
-            Pageable pageable);
 
 }
