6fe1983fbdb1d57a0e4aa557093a6d4f5450c384
andrehsvictor <andrehsvictor@gmail.com>
2024-12-08 10:09:00 -0300

    refactor: simplify UserRepository by removing findByUsernameOrEmail and adding findByEmail method

--- src/main/java/andrehsvictor/memorix/user/UserRepository.java
+++ src/main/java/andrehsvictor/memorix/user/UserRepository.java
@@ -6,18 +6,12 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    @Query("SELECT u FROM User u WHERE u.username = :usernameOrEmail OR u.email = :usernameOrEmail")
-    Optional<User> findByUsernameOrEmail(String usernameOrEmail);
-
-    boolean existsByUsername(String username);
+    Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
-
-    boolean existsByUsernameOrEmail(String username, String email);
 
     Page<User> findAllByDisplayNameOrUsernameContainingIgnoreCase(String displayName, String username,
             Pageable pageable);
