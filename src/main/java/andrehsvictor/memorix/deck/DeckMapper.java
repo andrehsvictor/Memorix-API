ae5288756d41374c7369b37226aa509386cc5a05
andrehsvictor <andrehsvictor@gmail.com>
2024-12-08 14:47:00 -0300

    refactor: implement DeckMapper for DTO conversions and null handling

---
+++ src/main/java/andrehsvictor/memorix/deck/DeckMapper.java
@@ -0,0 +1,41 @@
+package andrehsvictor.memorix.deck;
+
+import org.mapstruct.AfterMapping;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import andrehsvictor.memorix.deck.dto.GetDeckDto;
+import andrehsvictor.memorix.deck.dto.PostDeckDto;
+import andrehsvictor.memorix.deck.dto.PutDeckDto;
+
+@Mapper(componentModel = "spring")
+public interface DeckMapper {
+
+    GetDeckDto deckToGetDeckDto(Deck deck);
+
+    Deck postDeckDtoToDeck(PostDeckDto postDeckDto);
+
+    @AfterMapping
+    default void afterMapping(@MappingTarget Deck deck, PostDeckDto postDeckDto) {
+        if (postDeckDto.getAccentColor() == null) {
+            String randomColor = "#" + Integer.toHexString((int) (Math.random() * 0xffffff));
+            deck.setAccentColor(randomColor);
+        }
+    }
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Deck updateDeckFromPutDeckDto(PutDeckDto putDeckDto, @MappingTarget Deck deck);
+
+    @AfterMapping
+    default void afterMapping(@MappingTarget Deck deck, PutDeckDto putDeckDto) {
+        if (putDeckDto.getCoverUrl() != null && putDeckDto.getCoverUrl().isBlank()) {
+            deck.setCoverUrl(null);
+        }
+        if (putDeckDto.getDescription() != null && putDeckDto.getDescription().isBlank()) {
+            deck.setDescription(null);
+        }
+    }
+
+}
