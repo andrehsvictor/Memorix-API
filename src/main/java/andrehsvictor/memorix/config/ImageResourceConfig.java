1db7cb6cbc6fcf316244ea24e09c0787c6f32d51
andrehsvictor <andrehsvictor@gmail.com>
2024-12-17 16:00:51 -0300

    refactor: rename ImageConfig to ImageResourceConfig for clarity

--- src/main/java/andrehsvictor/memorix/config/ImageResourceConfig.java
+++ src/main/java/andrehsvictor/memorix/config/ImageResourceConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ImageConfig implements WebMvcConfigurer {
+public class ImageResourceConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
