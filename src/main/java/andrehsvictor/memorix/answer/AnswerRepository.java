f4780b281c180afdf8e5eb6b5ef155da6d253b3d
andrehsvictor <andrehsvictor@gmail.com>
2024-12-17 19:47:52 -0300

    refactor: remove Answer and Card entities along with their repositories and DTOs

--- src/main/java/andrehsvictor/memorix/answer/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package andrehsvictor.memorix.answer;
-
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, UUID> {
-
-}
--- src/main/java/andrehsvictor/memorix/card/CardTemplate.java
+++
@@ -1,9 +0,0 @@
-package andrehsvictor.memorix.card;
-
-public enum CardTemplate {
-    DEFAULT,
-    OBJECTIVE,
-    MULTIPLE_CHOICE,
-    TRUE_FALSE,
-    TEXT_INPUT
-}
