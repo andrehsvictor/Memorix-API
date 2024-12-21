3df737f974c76182b95f8d94aa871fdb74589c84
andrehsvictor <andrehsvictor@gmail.com>
2024-12-21 20:47:55 -0300

    feat: add Progress relationship to Card entity

--- src/main/java/andrehsvictor/memorix/card/Card.java
+++ src/main/java/andrehsvictor/memorix/card/Card.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import java.util.UUID;
 
 import andrehsvictor.memorix.deck.Deck;
+import andrehsvictor.memorix.progress.Progress;
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
@@ -17,6 +18,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
@@ -53,6 +55,9 @@ public class Card implements Serializable {
 
     @Enumerated(EnumType.STRING)
     private CardType type;
+
+    @OneToOne(mappedBy = "card")
+    private Progress progress;
 
     @ManyToOne
     @JoinColumn(name = "deck_id")
