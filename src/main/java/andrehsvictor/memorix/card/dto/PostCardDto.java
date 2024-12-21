4b9a5af55326dd1b0a4b5a8450c4fb8f55c9835a
andrehsvictor <andrehsvictor@gmail.com>
2024-12-21 15:26:33 -0300

    feat: update PostCardDto to use alternatives instead of options; change answer type and rename correctOptionIndex to answerIndex

--- src/main/java/andrehsvictor/memorix/card/dto/PostCardDto.java
+++ src/main/java/andrehsvictor/memorix/card/dto/PostCardDto.java
@@ -1,6 +1,6 @@
 package andrehsvictor.memorix.card.dto;
 
-import java.util.Set;
+import java.util.List;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -22,17 +22,13 @@ public class PostCardDto {
     @NotBlank(message = "Question is required")
     private String question;
 
-    @Size(min = 1, max = 1000, message = "Answer must be between 1 and 1000 characters")
-    private String answer;
+    private Object answer;
 
-    private Boolean booleanAnswer;
+    @Size(min = 2, max = 4, message = "Options must have at least 2 elements and at most 4")
+    private List<String> alternatives;
 
-    @Min(value = 2, message = "Options must have at least 2 elements")
-    @Max(value = 4, message = "Options must have at most 4 elements")
-    private Set<String> options;
+    @Min(value = 0, message = "Answer index must be between 0 and 3")
+    @Max(value = 3, message = "Answer index must be between 0 and 3")
+    private Integer answerIndex;
 
-    @Min(value = 0, message = "Correct option index must be between 0 and 3")
-    @Max(value = 3, message = "Correct option index must be between 0 and 3")
-    private Integer correctOptionIndex;
-    
 }
