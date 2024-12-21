ce0e87ed035802f99e95be9700434d8df42b686d
andrehsvictor <andrehsvictor@gmail.com>
2024-12-21 20:48:00 -0300

    feat: add review endpoint to CardResource for submitting reviews

--- src/main/java/andrehsvictor/memorix/card/CardResource.java
+++ src/main/java/andrehsvictor/memorix/card/CardResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import andrehsvictor.memorix.card.dto.GetCardDto;
 import andrehsvictor.memorix.card.dto.PostCardDto;
 import andrehsvictor.memorix.card.dto.PutCardDto;
+import andrehsvictor.memorix.card.dto.ReviewDto;
 import andrehsvictor.memorix.user.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -51,7 +53,7 @@ public class CardResource {
             @RequestBody @Valid PostCardDto postCardDto,
             @AuthenticationPrincipal User user) {
         GetCardDto getCardDto = cardMapper.cardToGetCardDto(
-                cardService.create(postCardDto, deckSlug, user.getId()));
+                cardService.create(postCardDto, deckSlug, user));
         URI location = URI.create("/v1/cards/" + getCardDto.getId());
         return ResponseEntity.created(location).body(getCardDto);
     }
@@ -66,6 +68,14 @@ public class CardResource {
     @DeleteMapping("/v1/cards/{id}")
     public ResponseEntity<Void> delete(@PathVariable UUID id, @AuthenticationPrincipal User user) {
         cardService.deleteByIdAndDeckUserId(id, user.getId());
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/v1/cards/{id}/review")
+    public ResponseEntity<Void> review(@PathVariable UUID id,
+            @RequestBody @Valid ReviewDto reviewDto,
+            @AuthenticationPrincipal User user) {
+        cardService.review(id, user.getId(), reviewDto);
         return ResponseEntity.noContent().build();
     }
 }
