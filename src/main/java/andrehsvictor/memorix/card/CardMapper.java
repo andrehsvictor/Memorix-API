22faf7faafe3456325569ccd0e4e28f645812889
andrehsvictor <andrehsvictor@gmail.com>
2024-12-21 10:47:15 -0300

    feat: add updateCardDtoFromPutCardDto method to CardMapper for card updates

--- src/main/java/andrehsvictor/memorix/card/CardMapper.java
+++ src/main/java/andrehsvictor/memorix/card/CardMapper.java
@@ -1,12 +1,16 @@
 package andrehsvictor.memorix.card;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 import andrehsvictor.memorix.card.dto.PostCardDto;
+import andrehsvictor.memorix.card.dto.PutCardDto;
 
 @Mapper(componentModel = "spring")
 public interface CardMapper {
 
     Card postCardDtoToCard(PostCardDto postCardDto);
-    
+
+    Card updateCardDtoFromPutCardDto(PutCardDto putCardDto, @MappingTarget Card card);
+
 }
