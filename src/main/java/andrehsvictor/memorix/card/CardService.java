37e8f4e83abdbbd7d049b51fa0a45ad6b5acc4b3
andrehsvictor <andrehsvictor@gmail.com>
2024-12-21 10:47:58 -0300

    feat: implement card creation, deletion, existence check, and update methods in CardService

--- src/main/java/andrehsvictor/memorix/card/CardService.java
+++ src/main/java/andrehsvictor/memorix/card/CardService.java
@@ -5,10 +5,10 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 import andrehsvictor.memorix.card.dto.PostCardDto;
+import andrehsvictor.memorix.card.dto.PutCardDto;
 import andrehsvictor.memorix.deck.Deck;
 import andrehsvictor.memorix.deck.DeckService;
 import andrehsvictor.memorix.exception.ResourceNotFoundException;
-import andrehsvictor.memorix.user.User;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -17,14 +17,14 @@ public class CardService {
 
     private final CardRepository cardRepository;
     private final DeckService deckService;
-    private final CardTypeValidatorFactory cardTypeValidatorFactory;
+    private final CardProcessorFactory cardProcessorFactory;
     private final CardMapper cardMapper;
 
-    public Card create(User user, String deckSlug, PostCardDto postCardDto) {
-        Deck deck = deckService.getBySlugAndUserId(deckSlug, user.getId());
+    public Card create(PostCardDto postCardDto, String deckSlug, UUID userId) {
+        Deck deck = deckService.getBySlugAndUserId(deckSlug, userId);
         Card card = cardMapper.postCardDtoToCard(postCardDto);
-        CardTypeValidator cardTypeValidator = cardTypeValidatorFactory.get(card.getType());
-        cardTypeValidator.validate(postCardDto);
+        CardProcessor cardProcessor = cardProcessorFactory.get(card.getType());
+        cardProcessor.process(card);
         card.setDeck(deck);
         return cardRepository.save(card);
     }
@@ -32,6 +32,28 @@ public class CardService {
     public Card getById(UUID id) {
         return cardRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Card not found with ID '" + id + "'"));
+    }
+
+    public void deleteByIdAndDeckUserId(UUID id, UUID userId) {
+        if (!existsByIdAndDeckUserId(id, userId)) {
+            throw new ResourceNotFoundException("Card not found with ID '" + id + "'");
+        }
+        cardRepository.deleteByIdAndDeckUserId(id, userId);
+    }
+
+    public boolean existsByIdAndDeckUserId(UUID id, UUID userId) {
+        return cardRepository.existsByIdAndDeckUserId(id, userId);
+    }
+
+    public Card update(UUID id, PutCardDto putCardDto, UUID userId) {
+        if (!existsByIdAndDeckUserId(id, userId)) {
+            throw new ResourceNotFoundException("Card not found with ID '" + id + "'");
+        }
+        Card card = getById(id);
+        cardMapper.updateCardDtoFromPutCardDto(putCardDto, card);
+        CardProcessor cardProcessor = cardProcessorFactory.get(card.getType());
+        cardProcessor.process(card);
+        return cardRepository.save(card);
     }
 
 }
