e98850bf406a3160603bdd227c80e597c6cf1488
andrehsvictor <andrehsvictor@gmail.com>
2024-12-21 10:47:52 -0300

    feat: add methods to check existence and delete cards by user ID in CardRepository

--- src/main/java/andrehsvictor/memorix/card/CardRepository.java
+++ src/main/java/andrehsvictor/memorix/card/CardRepository.java
@@ -6,4 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CardRepository extends JpaRepository<Card, UUID> {
 
+    boolean existsByIdAndDeckUserId(UUID cardId, UUID userId);
+
+    void deleteByIdAndDeckUserId(UUID cardId, UUID userId);
+
 }
