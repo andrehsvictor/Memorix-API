3380e4058a7a85c8d112ddd2c67feaf5be544ebe
andrehsvictor <andrehsvictor@gmail.com>
2024-12-21 09:14:53 -0300

    feat: add DeckCacheKeys class for caching deck-related data

---
+++ src/main/java/andrehsvictor/memorix/deck/DeckCacheKeys.java
@@ -0,0 +1,8 @@
+package andrehsvictor.memorix.deck;
+
+public class DeckCacheKeys {
+    public static final String DECK__USER_ID__RESULT_SLUG = "'deck:' + #user.id + ':' + #result.slug";
+    public static final String DECK__USER_ID = "'deck:' + #user.id";
+    public static final String DECK__USERID__SLUG = "'deck:' + #userId + ':' + #slug";
+    public static final String DECK__USERID = "'deck:' + #userId";
+}
