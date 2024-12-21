29a21ec958f256919002d1f67a80b2e2a003c05d
andrehsvictor <andrehsvictor@gmail.com>
2024-12-21 15:26:48 -0300

    feat: update PutCardDto to use alternatives instead of options; change answer type and rename correctOptionIndex to answerIndex

--- src/main/java/andrehsvictor/memorix/card/dto/PutCardDto.java
+++ src/main/java/andrehsvictor/memorix/card/dto/PutCardDto.java
@@ -16,16 +16,14 @@ public class PutCardDto {
     @Size(min = 1, max = 1000, message = "Question must be between 1 and 1000 characters")
     private String question;
 
-    @Size(min = 1, max = 1000, message = "Answer must be between 1 and 1000 characters")
-    private String answer;
+    private Object answer;
 
-    @Min(value = 2, message = "Options must have at least 2 elements")
-    @Max(value = 4, message = "Options must have at most 4 elements")
-    private Set<String> options;
+    @Size(min = 2, max = 4, message = "Options must have at least 2 elements and at most 4")
+    private Set<String> alternatives;
 
     @Min(value = 0, message = "Correct option index must be between 0 and 3")
     @Max(value = 3, message = "Correct option index must be between 0 and 3")
-    private Integer correctOptionIndex;
+    private Integer answerIndex;
 
     private Boolean booleanAnswer;
 
