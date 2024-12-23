b80a31b2ba22645970186f6b59551b75a665a1c6
andrehsvictor <andrehsvictor@gmail.com>
2024-12-23 14:10:30 -0300

    feat: add methods to count and retrieve cards based on user's next repetition date

--- src/main/java/andrehsvictor/memorix/card/CardRepository.java
+++ src/main/java/andrehsvictor/memorix/card/CardRepository.java
@@ -1,5 +1,6 @@
 package andrehsvictor.memorix.card;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -18,5 +19,10 @@ public interface CardRepository extends JpaRepository<Card, UUID> {
     Page<Card> findAllByDeckUserId(UUID userId, Pageable pageable);
 
     Page<Card> findAllByDeckUserIdAndDeckId(UUID userId, UUID deckId, Pageable pageable);
+
+    Integer countByDeckUserIdAndProgressNextRepetitionBefore(UUID userId, LocalDateTime nextRepetition);
+
+    Page<Card> findAllByDeckUserIdAndProgressNextRepetitionBefore(UUID userId, LocalDateTime nextRepetition,
+            Pageable pageable);
 
 }
