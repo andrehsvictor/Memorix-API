9684c7176ffa2e789e0b8f0584c32c17c48cae8d
andrehsvictor <andrehsvictor@gmail.com>
2024-12-23 14:10:17 -0300

    feat: add endpoint to retrieve progress by card ID for authenticated users

--- src/main/java/andrehsvictor/memorix/progress/ProgressResource.java
+++ src/main/java/andrehsvictor/memorix/progress/ProgressResource.java
@@ -2,10 +2,9 @@ package andrehsvictor.memorix.progress;
 
 import java.util.UUID;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import andrehsvictor.memorix.progress.dto.GetProgressDto;
@@ -18,10 +17,11 @@ public class ProgressResource {
     private final ProgressService progressService;
     private final ProgressMapper progressMapper;
 
-    @GetMapping("/v1/progresses")
-    public Page<GetProgressDto> getAll(@AuthenticationPrincipal UUID userId, Pageable pageable) {
-        Page<Progress> progresses = progressService.getAllByUserId(userId, pageable);
-        Page<GetProgressDto> getProgressDtos = progresses.map(progressMapper::progressToGetProgressDto);
-        return getProgressDtos;
+    @GetMapping("/v1/cards/{cardId}/progress")
+    public GetProgressDto getByCardId(@AuthenticationPrincipal UUID userId, @PathVariable UUID cardId) {
+        Progress progress = progressService.getByUserIdAndCardId(userId, cardId);
+        GetProgressDto getProgressDto = progressMapper.progressToGetProgressDto(progress);
+        return getProgressDto;
     }
+
 }
