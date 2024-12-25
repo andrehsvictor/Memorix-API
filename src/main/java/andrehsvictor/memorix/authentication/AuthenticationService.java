b80ba41cbe4a04b6b4774d45a59e2c8d55fdaee1
andrehsvictor <andrehsvictor@gmail.com>
2024-12-25 14:19:32 -0300

    feat: enhance authentication process with improved error handling for user login

--- src/main/java/andrehsvictor/memorix/authentication/AuthenticationService.java
+++ src/main/java/andrehsvictor/memorix/authentication/AuthenticationService.java
@@ -1,10 +1,13 @@
 package andrehsvictor.memorix.authentication;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import andrehsvictor.memorix.exception.UnauthorizedException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -14,10 +17,18 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
     private final EmailVerificationService emailVerificationService;
 
-    public Authentication authenticate(String username, String password) {
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+    public Authentication authenticate(String email, String password) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email,
                 password);
-        return authenticationManager.authenticate(authenticationToken);
+        try {
+            return authenticationManager.authenticate(authenticationToken);
+        } catch (DisabledException e) {
+            throw new UnauthorizedException("User account is not fully activated");
+        } catch (BadCredentialsException e) {
+            throw new UnauthorizedException("Invalid e-mail or password");
+        } catch (Exception e) {
+            throw new UnauthorizedException("An error occurred while trying to authenticate the user");
+        }
     }
 
     public void sendVerificationEmail(String email) {
