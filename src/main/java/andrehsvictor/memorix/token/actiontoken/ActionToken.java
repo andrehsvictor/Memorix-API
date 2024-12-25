5629ef037614db81352b587c6d8247fffc76098c
andrehsvictor <andrehsvictor@gmail.com>
2024-12-25 09:29:06 -0300

    feat: enhance ActionToken model with indexed token field and equals/hashCode implementation

--- src/main/java/andrehsvictor/memorix/token/actiontoken/ActionToken.java
+++ src/main/java/andrehsvictor/memorix/token/actiontoken/ActionToken.java
@@ -5,18 +5,21 @@ import java.util.UUID;
 
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
 
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @RedisHash("action_token")
+@EqualsAndHashCode(of = "id")
 public class ActionToken implements Serializable {
 
     private static final long serialVersionUID = 8803182935029479370L;
@@ -25,6 +28,7 @@ public class ActionToken implements Serializable {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Indexed
     private String token;
 
     @Enumerated(EnumType.STRING)
