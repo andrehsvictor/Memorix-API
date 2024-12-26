40a0077ab350b9283eaa01ee010e73f04cb5d2a3
andrehsvictor <andrehsvictor@gmail.com>
2024-12-26 09:32:56 -0300

    feat: enhance MinioService to use configurable bucket name and URL for uploads

--- src/main/java/andrehsvictor/memorix/minio/MinioService.java
+++ src/main/java/andrehsvictor/memorix/minio/MinioService.java
@@ -1,9 +1,9 @@
 package andrehsvictor.memorix.minio;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import io.minio.GetPresignedObjectUrlArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import lombok.RequiredArgsConstructor;
@@ -14,26 +14,30 @@ public class MinioService {
 
     private final MinioClient minioClient;
 
-    private static final String BUCKET_NAME = "memorix";
+    @Value("${minio.default-bucket:memorix}")
+    private String defaultBucket = "memorix";
+
+    @Value("${minio.url:http://localhost:9000}")
+    private String url = "http://localhost:9000";
 
     public String upload(MultipartFile file) {
         try {
             String filename = generateFilename(file);
             PutObjectArgs args = PutObjectArgs.builder()
-                    .bucket(BUCKET_NAME)
+                    .bucket(defaultBucket)
                     .object(filename)
                     .stream(file.getInputStream(), file.getSize(), -1)
                     .contentType(file.getContentType())
                     .build();
             minioClient.putObject(args);
-            GetPresignedObjectUrlArgs urlArgs = GetPresignedObjectUrlArgs.builder()
-                    .bucket(BUCKET_NAME)
-                    .object(filename)
-                    .build();
-            return minioClient.getPresignedObjectUrl(urlArgs);
+            return generateUrl(filename);
         } catch (Exception e) {
             throw new RuntimeException("Failed to upload file", e);
         }
+    }
+
+    private String generateUrl(String filename) {
+        return url + "/" + defaultBucket + "/" + filename;
     }
 
     private String generateFilename(MultipartFile file) {
