62547391c16f0fe388b10e54309b4fef163aca7d
andrehsvictor <andrehsvictor@gmail.com>
2024-12-26 09:33:01 -0300

    feat: add validation for MinIO root user and password, and create default bucket with public-read policy

--- src/main/java/andrehsvictor/memorix/config/MinioConfig.java
+++ src/main/java/andrehsvictor/memorix/config/MinioConfig.java
@@ -3,26 +3,56 @@ package andrehsvictor.memorix.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import io.minio.SetBucketPolicyArgs;
+import jakarta.validation.constraints.Size;
 
 @Configuration
+@Validated
 public class MinioConfig {
 
     @Value("${minio.url:http://localhost:9000}")
     private String endpoint = "http://localhost:9000";
 
+    @Size(min = 3, message = "Root username must be at least 3 characters long")
     @Value("${minio.root-user:root}")
     private String rootUser = "root";
 
+    @Size(min = 8, message = "Root password must be at least 8 characters long")
     @Value("${minio.root-password:root}")
     private String rootPassword = "root";
 
+    @Value("${minio.default-bucket:memorix}")
+    private String defaultBucket = "memorix";
+
     @Bean
     MinioClient minioClient() {
-        return MinioClient.builder()
+        MinioClient minioClient = MinioClient.builder()
                 .endpoint(endpoint)
                 .credentials(rootUser, rootPassword)
                 .build();
+        try {
+            BucketExistsArgs args = BucketExistsArgs.builder()
+                    .bucket(defaultBucket)
+                    .build();
+            if (!minioClient.bucketExists(args)) {
+                MakeBucketArgs makeBucketArgs = MakeBucketArgs.builder()
+                        .bucket(defaultBucket)
+                        .build();
+                minioClient.makeBucket(makeBucketArgs);
+            }
+            SetBucketPolicyArgs policyArgs = SetBucketPolicyArgs.builder()
+                    .bucket(defaultBucket)
+                    .config("public-read")
+                    .build();
+            minioClient.setBucketPolicy(policyArgs);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create default bucket", e);
+        }
+        return minioClient;
     }
 }
