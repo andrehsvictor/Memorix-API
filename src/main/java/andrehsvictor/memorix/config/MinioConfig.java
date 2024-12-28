0dc40678d2c9507e72d9d55b3e7b0c9f3d9c863b
andrehsvictor <andrehsvictor@gmail.com>
2024-12-28 09:45:19 -0300

    fix: update MinioConfig to change default root password and remove unused FileService

--- src/main/java/andrehsvictor/memorix/config/MinioConfig.java
+++ src/main/java/andrehsvictor/memorix/config/MinioConfig.java
@@ -5,11 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
-import andrehsvictor.memorix.file.FileService;
 import io.minio.BucketExistsArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
-import io.minio.SetBucketPolicyArgs;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 
@@ -26,13 +24,11 @@ public class MinioConfig {
     private String rootUser = "root";
 
     @Size(min = 8, message = "Root password must be at least 8 characters long")
-    @Value("${minio.root-password:root}")
-    private String rootPassword = "root";
+    @Value("${minio.root-password:rootadmin}")
+    private String rootPassword = "rootadmin";
 
     @Value("${minio.default-bucket:memorix}")
     private String defaultBucket = "memorix";
-
-    private final FileService fileService;
 
     @Bean
     MinioClient minioClient() {
@@ -50,13 +46,6 @@ public class MinioConfig {
                         .build();
                 minioClient.makeBucket(makeBucketArgs);
             }
-            String config = fileService.read("classpath:static/json/bucket-policy.json");
-            config = config.replace("{{bucketName}}", defaultBucket);
-            SetBucketPolicyArgs policyArgs = SetBucketPolicyArgs.builder()
-                    .bucket(defaultBucket)
-                    .config(config)
-                    .build();
-            minioClient.setBucketPolicy(policyArgs);
         } catch (Exception e) {
             throw new RuntimeException("Failed to create default bucket", e);
         }
