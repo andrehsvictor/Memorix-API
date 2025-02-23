fc879dd0b9b35dcbc676f14d6df4b9a7fc7988e9
andrehsvictor <you@example.com>
2025-02-23 10:45:38 -0300

    feat: initialize Spring Boot project with basic structure and configuration

---
+++ src/test/java/andrehsvictor/memorix/MemorixApplicationTests.java
@@ -0,0 +1,13 @@
+package andrehsvictor.memorix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
