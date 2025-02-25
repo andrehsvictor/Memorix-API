65ecb2dc53cad6548db309c679509b0ace7a67bb
andrehsvictor <you@example.com>
2025-02-25 15:22:13 -0300

    feat: update RSA key generation script to use .key file extensions

--- rsa.sh
+++ rsa.sh
@@ -1,18 +1,18 @@
 mkdir -p .keys && cd .keys
 
-if [ ! -f private.pem ] && [ -f public.pem ]; then
+if [ ! -f private.key ] && [ -f public.key ]; then
     echo "Found only public key. Regenerating RSA key pair..."
-    rm -f public.pem
-    rm -f private.pem
-    openssl genrsa -out private.pem 2048
-    openssl rsa -in private.pem -pubout -out public.pem
-elif [ -f private.pem ] && [ ! -f public.pem ]; then
+    rm -f public.key
+    rm -f private.key
+    openssl genrsa -out private.key 2048
+    openssl rsa -in private.key -pubout -out public.key
+elif [ -f private.key ] && [ ! -f public.key ]; then
     echo "Found only private key. Generating public key..."
-    openssl rsa -in private.pem -pubout -out public.pem
-elif [ ! -f private.pem ] && [ ! -f public.pem ]; then
+    openssl rsa -in private.key -pubout -out public.key
+elif [ ! -f private.key ] && [ ! -f public.key ]; then
     echo "Generating RSA key pair..."
-    openssl genrsa -out private.pem 2048
-    openssl rsa -in private.pem -pubout -out public.pem
+    openssl genrsa -out private.key 2048
+    openssl rsa -in private.key -pubout -out public.key
 else
     echo "RSA key pair already exists."
 fi
