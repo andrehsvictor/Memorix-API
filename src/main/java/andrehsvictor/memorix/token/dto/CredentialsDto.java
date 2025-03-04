a3dc146ddd5f0dd9c79edd415d333b99b88ab9f6
andrehsvictor <andrehsvictor@gmail.com>
2025-03-04 12:03:38 -0300

    feat: add validation annotations to CredentialsDto for username and password

--- src/main/java/andrehsvictor/memorix/token/dto/CredentialsDto.java
+++ src/main/java/andrehsvictor/memorix/token/dto/CredentialsDto.java
@@ -1,11 +1,17 @@
 package andrehsvictor.memorix.token.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CredentialsDto {
+
+    @NotEmpty(message = "Username is required")
     private String username;
+
+    @NotEmpty(message = "Password is required")
     private String password;
+    
 }
