3fa2925b803ba0ff48089610e4357fd6d33c68ff
andrehsvictor <andrehsvictor@gmail.com>
2025-03-04 09:58:04 -0300

    feat: add ResourceNotFoundException and UnauthorizedException for improved error handling

---
+++ src/main/java/andrehsvictor/memorix/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package andrehsvictor.memorix.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = -1251993477145024542L;
+
+    public ResourceNotFoundException(Class<?> clazz, String key, Object value) {
+        super(String.format("%s not found with '%s': %s", clazz.getSimpleName(), key, value));
+    }
+
+}
---
+++ src/main/java/andrehsvictor/memorix/exception/UnauthorizedException.java
@@ -0,0 +1,13 @@
+package andrehsvictor.memorix.exception;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class UnauthorizedException extends AuthenticationException {
+
+    private static final long serialVersionUID = -5989038234756256476L;
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+}
