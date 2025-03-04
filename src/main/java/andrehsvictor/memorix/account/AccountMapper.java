99d42b2f1a30c4449c2fb871a3d197d013116e6c
andrehsvictor <andrehsvictor@gmail.com>
2025-03-04 08:31:13 -0300

    feat: add mapping method for converting CreateAccountDto to User

--- src/main/java/andrehsvictor/memorix/account/AccountMapper.java
+++ src/main/java/andrehsvictor/memorix/account/AccountMapper.java
@@ -3,11 +3,14 @@ package andrehsvictor.memorix.account;
 import org.mapstruct.Mapper;
 
 import andrehsvictor.memorix.account.dto.AccountDto;
+import andrehsvictor.memorix.account.dto.CreateAccountDto;
 import andrehsvictor.memorix.user.User;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
 
     AccountDto userToAccountDto(User user);
+
+    User createAccountDtoToUser(CreateAccountDto createAccountDto);
 
 }
