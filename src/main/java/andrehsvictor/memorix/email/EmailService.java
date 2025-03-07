927bea0cfd3e0bbd54d6df2650d5c2cb12f6ce25
andrehsvictor <andrehsvictor@gmail.com>
2025-03-07 16:32:39 -0300

    feat: add EmailService for sending emails with JavaMailSender

---
+++ src/main/java/andrehsvictor/memorix/email/EmailService.java
@@ -0,0 +1,32 @@
+package andrehsvictor.memorix.email;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    private final static String FROM = "Memorix";
+
+    public void send(String to, String subject, String text) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(FROM);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);
+            javaMailSender.send(message);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
+    }
+
+}
