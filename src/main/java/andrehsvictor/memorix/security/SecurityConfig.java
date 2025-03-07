53fdce80853f8e77ca3b870d5862c39030c06bc7
andrehsvictor <andrehsvictor@gmail.com>
2025-03-07 16:33:26 -0300

    feat: enhance security configuration to allow specific API paths for POST and DELETE methods

--- src/main/java/andrehsvictor/memorix/security/SecurityConfig.java
+++ src/main/java/andrehsvictor/memorix/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -25,6 +27,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtDecoder jwtDecoder;
+
     @Value("${cors.allowed-origins:*}")
     private String[] allowedOrigins = { "*" };
 
@@ -34,7 +38,20 @@ public class SecurityConfig {
     private static final String[] ALLOWED_PATHS_WITH_ANY_METHOD = {
             "/swagger-ui/**",
             "/v3/api-docs/**",
-            "/swagger-ui.html"
+            "/swagger-ui.html",
+            "/account/verify"
+    };
+
+    private static final String[] ALLOWED_PATHS_WITH_POST_METHOD = {
+            "/api/v1/token",
+            "/api/v1/token/refresh",
+            "/api/v1/account",
+            "/api/v1/account/send-action-email",
+            "/api/v1/account/verify"
+    };
+
+    private static final String[] ALLOWED_PATHS_WITH_DELETE_METHOD = {
+            "/api/v1/token"
     };
 
     @Bean
@@ -43,8 +60,11 @@ public class SecurityConfig {
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authorizeHttpRequests(authorize -> {
             authorize.requestMatchers(ALLOWED_PATHS_WITH_ANY_METHOD).permitAll();
+            authorize.requestMatchers(HttpMethod.POST, ALLOWED_PATHS_WITH_POST_METHOD).permitAll();
+            authorize.requestMatchers(HttpMethod.DELETE, ALLOWED_PATHS_WITH_DELETE_METHOD).permitAll();
             authorize.anyRequest().authenticated();
         });
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder)));
         return http.build();
     }
 
