2daf74415ce3cd0f39f92986444fa2823035a1cd
andrehsvictor <andrehsvictor@gmail.com>
2025-03-08 08:44:01 -0300

    fix: improve error message formatting in ResourceNotFoundException

--- src/main/java/andrehsvictor/memorix/exception/ResourceNotFoundException.java
+++ src/main/java/andrehsvictor/memorix/exception/ResourceNotFoundException.java
@@ -5,7 +5,7 @@ public class ResourceNotFoundException extends RuntimeException {
     private static final long serialVersionUID = -1251993477145024542L;
 
     public ResourceNotFoundException(Class<?> clazz, String key, Object value) {
-        super(String.format("%s not found with '%s': %s", clazz.getSimpleName(), key, value));
+        super(String.format("%s not found with %s: %s", clazz.getSimpleName(), key, value));
     }
 
     public ResourceNotFoundException(String message) {
