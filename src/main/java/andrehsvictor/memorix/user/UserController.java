07c53bd05a768039568c1c9990d08d83ee2f3349
andrehsvictor <andrehsvictor@gmail.com>
2025-03-08 10:14:39 -0300

    feat: add UserController to handle user retrieval endpoints

---
+++ src/main/java/andrehsvictor/memorix/user/UserController.java
@@ -0,0 +1,29 @@
+package andrehsvictor.memorix.user;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import andrehsvictor.memorix.user.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @GetMapping("/api/v1/users")
+    public Page<UserDto> findAll(@RequestParam(required = false) String query, Pageable pageable) {
+        return userService.findAll(query, pageable).map(userMapper::userToUserDto);
+    }
+
+    @GetMapping("/api/v1/users/{id}")
+    public UserDto findById(Long id) {
+        return userMapper.userToUserDto(userService.findById(id));
+    }
+
+}
