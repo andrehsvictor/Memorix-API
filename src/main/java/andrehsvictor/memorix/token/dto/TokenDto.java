84fda20cce2954190b1acb9d772f355fef3b1223
andrehsvictor <andrehsvictor@gmail.com>
2025-03-09 16:29:15 -0300

    feat: add validation annotations to TokenDto for JWT token requirements

--- src/main/java/andrehsvictor/memorix/token/dto/TokenDto.java
+++ src/main/java/andrehsvictor/memorix/token/dto/TokenDto.java
@@ -1,8 +1,14 @@
 package andrehsvictor.memorix.token.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class TokenDto {
+
+    @NotBlank(message = "Token is required")
+    @Pattern(message = "Invalid JWT token", regexp = "^[a-zA-Z0-9-_=]+\\.[a-zA-Z0-9-_=]+\\.[a-zA-Z0-9-_.+/=]*$")
     private String token;
+
 }
