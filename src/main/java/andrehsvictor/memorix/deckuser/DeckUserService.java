7f1060bf0e70626d977b8be297d2e1e5d6c85a40
andrehsvictor <andrehsvictor@gmail.com>
2025-03-14 11:11:37 -0300

    feat: enhance DeckUserService with access management methods and user-deck association functionality

--- src/main/java/andrehsvictor/memorix/deckuser/DeckUserService.java
+++ src/main/java/andrehsvictor/memorix/deckuser/DeckUserService.java
@@ -2,6 +2,10 @@ package andrehsvictor.memorix.deckuser;
 
 import org.springframework.stereotype.Service;
 
+import andrehsvictor.memorix.deck.Deck;
+import andrehsvictor.memorix.deck.DeckService;
+import andrehsvictor.memorix.user.User;
+import andrehsvictor.memorix.user.UserService;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -9,9 +13,29 @@ import lombok.RequiredArgsConstructor;
 public class DeckUserService {
 
     private final DeckUserRepository deckUserRepository;
+    private final UserService userService;
+    private final DeckService deckService;
 
-    public boolean existsByUserIdAndDeckIdAndAccessLevel(Long userId, Long deckId, AccessLevel accessLevel) {
+    public boolean hasAccessLevel(Long userId, Long deckId, AccessLevel accessLevel) {
         return deckUserRepository.existsByUserIdAndDeckIdAndAccessLevel(userId, deckId, accessLevel);
+    }
+
+    public boolean hasAccess(Long userId, Long deckId) {
+        return deckUserRepository.existsByUserIdAndDeckId(userId, deckId);
+    }
+
+    public DeckUser create(Long userId, Long deckId, AccessLevel accessLevel) {
+        DeckUser deckUser = new DeckUser();
+        User user = userService.findById(userId);
+        Deck deck = deckService.findById(deckId);
+        deckUser.setUser(user);
+        deckUser.setDeck(deck);
+        deckUser.setAccessLevel(accessLevel);
+        return deckUserRepository.save(deckUser);
+    }
+
+    public void delete(Long userId, Long deckId) {
+        deckUserRepository.deleteByUserIdAndDeckId(userId, deckId);
     }
 
 }
