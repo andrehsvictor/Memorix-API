1466e5d17e58f3dac24e7ef0229bfe1cb8b21ac9
andrehsvictor <andrehsvictor@gmail.com>
2025-03-21 16:19:38 -0300

    feat: add @NoArgsConstructor and @AllArgsConstructor annotations to CreateUserDto for enhanced object initialization

--- src/main/java/andrehsvictor/memorix/user/dto/CreateUserDto.java
+++ src/main/java/andrehsvictor/memorix/user/dto/CreateUserDto.java
@@ -4,11 +4,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateUserDto {
 
     @NotEmpty(message = "Username is required")
