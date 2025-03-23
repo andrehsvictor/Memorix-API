dfb118f3c6d1fd88678d99cde5ce13c3bc246980
andrehsvictor <andrehsvictor@gmail.com>
2025-03-23 09:59:22 -0300

    test: refactor IntegrationTest to use alpine images for PostgreSQL and Redis containers

--- src/test/java/andrehsvictor/memorix/IntegrationTest.java
+++ src/test/java/andrehsvictor/memorix/IntegrationTest.java
@@ -7,14 +7,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-
-import com.redis.testcontainers.RedisContainer;
 
 import io.restassured.RestAssured;
 
@@ -25,15 +22,15 @@ public abstract class IntegrationTest {
     @Container
     @SuppressWarnings("resource")
     private static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER = new PostgreSQLContainer<>(
-            DockerImageName.parse("postgres:latest"))
+            DockerImageName.parse("postgres:alpine"))
             .withDatabaseName("memorix")
             .withUsername("memorix")
             .withPassword("memorix");
 
     @Container
     @SuppressWarnings("resource")
-    private static final RedisContainer REDIS_CONTAINER = new RedisContainer(
-            DockerImageName.parse("redis:latest"))
+    private static final GenericContainer<?> REDIS_CONTAINER = new GenericContainer<>(
+            DockerImageName.parse("redis:alpine"))
             .withExposedPorts(6379);
 
     @LocalServerPort
@@ -44,22 +41,20 @@ public abstract class IntegrationTest {
         POSTGRESQL_CONTAINER.start();
         REDIS_CONTAINER.start();
 
+        System.setProperty("spring.datasource.url", POSTGRESQL_CONTAINER.getJdbcUrl());
+        System.setProperty("spring.datasource.username", POSTGRESQL_CONTAINER.getUsername());
+        System.setProperty("spring.datasource.password", POSTGRESQL_CONTAINER.getPassword());
+        System.setProperty("spring.data.redis.host", REDIS_CONTAINER.getHost());
+        System.setProperty("spring.data.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
+
         assertThat(POSTGRESQL_CONTAINER.isRunning()).isTrue();
         assertThat(REDIS_CONTAINER.isRunning()).isTrue();
-    }
-
-    @DynamicPropertySource
-    static void configure(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRESQL_CONTAINER::getUsername);
-        registry.add("spring.datasource.password", POSTGRESQL_CONTAINER::getPassword);
-        registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
-        registry.add("spring.data.redis.port", REDIS_CONTAINER::getFirstMappedPort);
     }
 
     @BeforeEach
     void setUp() {
         RestAssured.baseURI = String.format("http://localhost:%d", port);
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     }
 
 }
