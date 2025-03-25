40e0829339806f7d18e59df5f360df1a3b2a75c4
andrehsvictor <andrehsvictor@gmail.com>
2025-03-25 16:04:58 -0300

    feat: add Builder annotation to CredentialsDto for improved object creation

--- src/main/java/andrehsvictor/memorix/token/dto/CredentialsDto.java
+++ src/main/java/andrehsvictor/memorix/token/dto/CredentialsDto.java
@@ -1,9 +1,11 @@
 package andrehsvictor.memorix.token.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class CredentialsDto {
 
     @NotEmpty(message = "Username is required")
