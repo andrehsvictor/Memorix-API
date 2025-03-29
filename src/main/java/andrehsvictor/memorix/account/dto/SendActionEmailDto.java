30525ff4456d9b457c5f4106b821731b457a845c
andrehsvictor <andrehsvictor@gmail.com>
2025-03-29 10:49:55 -0300

    refactor: update validation annotations in SendActionEmailDto for improved clarity

--- src/main/java/andrehsvictor/memorix/account/dto/SendActionEmailDto.java
+++ src/main/java/andrehsvictor/memorix/account/dto/SendActionEmailDto.java
@@ -1,7 +1,7 @@
 package andrehsvictor.memorix.account.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
@@ -10,15 +10,15 @@ import lombok.Data;
 @Builder
 public class SendActionEmailDto {
 
-    @Pattern(regexp = "^(RESET_PASSWORD|VERIFY_EMAIL)$", message = "Type is invalid")
-    @NotEmpty(message = "Type is required")
+    @Pattern(regexp = "^(RESET_PASSWORD|VERIFY_EMAIL)$", message = "Type must be either RESET_PASSWORD or VERIFY_EMAIL")
+    @NotBlank(message = "Type is required")
     private String type;
 
-    @NotEmpty(message = "Email is required")
+    @NotBlank(message = "Email is required")
     @Email(message = "Email is invalid")
     private String email;
 
-    @NotEmpty(message = "Redirect URL is required")
+    @NotBlank(message = "Redirect URL is required")
     @Pattern(regexp = "^(http|https)://.*$", message = "Redirect URL is invalid")
     private String redirectUrl;
 }
