ff7e7194dbee230e4fd312b02110fa0baccd6bcb
andrehsvictor <andrehsvictor@gmail.com>
2025-03-29 10:50:42 -0300

    feat: add IntegrationTestBeansConfig for mocking EmailService in integration tests

---
+++ src/test/java/andrehsvictor/memorix/IntegrationTestBeansConfig.java
@@ -0,0 +1,18 @@
+package andrehsvictor.memorix;
+
+import static org.mockito.Mockito.mock;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import andrehsvictor.memorix.email.EmailService;
+
+@Configuration
+public class IntegrationTestBeansConfig {
+
+    @Bean
+    EmailService emailService() {
+        return mock(EmailService.class);
+    }
+
+}
