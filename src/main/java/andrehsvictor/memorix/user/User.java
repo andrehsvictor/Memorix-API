856953dbdd4b82389ff06807454aacaa4147e8c9
andrehsvictor <andrehsvictor@gmail.com>
2025-06-08 15:48:00 -0300

    feat: implement User entity with fields for user management

---
+++ src/main/java/andrehsvictor/memorix/user/User.java
@@ -0,0 +1,85 @@
+package andrehsvictor.memorix.user;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+@EqualsAndHashCode(of = { "id", "username", "email" })
+@ToString(exclude = { "password", "emailVerificationToken", "passwordResetToken" })
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -7572304554717990729L;
+
+    @Column(name = "id", nullable = false, unique = true)
+    private UUID id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(name = "display_name", nullable = false, length = 100)
+    private String displayName;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "email_verified", nullable = false)
+    private boolean emailVerified;
+
+    @Column(name = "password_hash", length = 255)
+    private String password;
+
+    @Column(name = "bio", columnDefinition = "TEXT")
+    private String bio;
+
+    @Column(name = "picture_url", length = 255)
+    private String pictureUrl;
+
+    // TODO: Add an Enum for provider
+    @Column(name = "provider", nullable = false, length = 50)
+    private String provider;
+
+    @Column(name = "provider_id", nullable = false, length = 100)
+    private String providerId;
+
+    @Column(name = "email_verification_token", length = 255)
+    private String emailVerificationToken;
+
+    @Column(name = "email_verification_token_expires_at")
+    private LocalDateTime emailVerificationTokenExpiresAt;
+
+    @Column(name = "password_reset_token", length = 255)
+    private String passwordResetToken;
+
+    @Column(name = "password_reset_token_expires_at")
+    private LocalDateTime passwordResetTokenExpiresAt;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+}
