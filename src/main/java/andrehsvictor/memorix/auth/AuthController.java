73d05030474cfa10fb8b230d2a3ce97ec6216a87
andrehsvictor <andrehsvictor@gmail.com>
2025-06-11 15:20:01 -0300

    feat: implement AuthController for token management and authentication endpoints

---
+++ src/main/java/andrehsvictor/memorix/auth/AuthController.java
@@ -0,0 +1,41 @@
+package andrehsvictor.memorix.auth;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import andrehsvictor.memorix.auth.dto.CredentialsDto;
+import andrehsvictor.memorix.auth.dto.IdTokenDto;
+import andrehsvictor.memorix.auth.dto.RefreshTokenDto;
+import andrehsvictor.memorix.auth.dto.RevokeTokenDto;
+import andrehsvictor.memorix.auth.dto.TokenDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final TokenService tokenService;
+
+    @PostMapping("/api/v1/auth/token")
+    public TokenDto requestToken(@Valid @RequestBody CredentialsDto credentialsDto) {
+        return tokenService.request(credentialsDto);
+    }
+
+    @PostMapping("/api/v1/auth/google")
+    public TokenDto googleAuth(@Valid @RequestBody IdTokenDto idTokenDto) {
+        return tokenService.google(idTokenDto);
+    }
+
+    @PostMapping("/api/v1/auth/refresh")
+    public TokenDto refreshToken(@Valid @RequestBody RefreshTokenDto refreshTokenDto) {
+        return tokenService.refresh(refreshTokenDto);
+    }
+
+    @PostMapping("/api/v1/auth/revoke")
+    public void revokeToken(@Valid @RequestBody RevokeTokenDto revokeTokenDto) {
+        tokenService.revoke(revokeTokenDto);
+    }
+
+}
