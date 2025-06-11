0c9bce3d456d7f1155329cecf4d08ff63c3fb9ab
andrehsvictor <andrehsvictor@gmail.com>
2025-06-11 09:51:12 -0300

    feat: add custom exception classes and Redis configuration for token management

--- src/main/java/andrehsvictor/memorix/common/jwt/JwtService.java
+++ src/main/java/andrehsvictor/memorix/common/jwt/JwtService.java
@@ -1,7 +1,13 @@
 package andrehsvictor.memorix.common.jwt;
 
+import java.time.Instant;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -12,5 +18,31 @@ public class JwtService {
 
     private final JwtEncoder jwtEncoder;
     private final JwtDecoder jwtDecoder;
+    private final JwtLifetimeProperties jwtLifetimeProperties;
 
+    public Jwt issueAccessToken(UserDetails userDetails) {
+        String subject = userDetails.getUsername();
+        Instant now = Instant.now();
+        Instant expiresAt = now.plus(jwtLifetimeProperties.getAccessTokenLifetime());
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .subject(subject)
+                .issuedAt(now)
+                .expiresAt(expiresAt)
+                .claim("type", "access")
+                .build();
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims));
+    }
+
+    public Jwt issueRefreshToken(UserDetails userDetails) {
+        String subject = userDetails.getUsername();
+        Instant now = Instant.now();
+        Instant expiresAt = now.plus(jwtLifetimeProperties.getRefreshTokenLifetime());
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .subject(subject)
+                .issuedAt(now)
+                .expiresAt(expiresAt)
+                .claim("type", "refresh")
+                .build();
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims));
+    }
 }
