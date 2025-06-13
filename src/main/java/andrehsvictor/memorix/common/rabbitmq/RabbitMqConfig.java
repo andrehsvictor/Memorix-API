127714b87436443714a2f5046b1993a99ab5d791
andrehsvictor <andrehsvictor@gmail.com>
2025-06-13 16:13:35 -0300

    feat: add RabbitMqConfig class for RabbitMQ configuration

---
+++ src/main/java/andrehsvictor/memorix/common/rabbitmq/RabbitMqConfig.java
@@ -0,0 +1,16 @@
+package andrehsvictor.memorix.common.rabbitmq;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setAutoStartup(true);
+        return rabbitAdmin;
+    }
+
+}
