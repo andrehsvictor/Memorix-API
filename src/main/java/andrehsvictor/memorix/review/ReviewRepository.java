f1c65944ab812c47f5e7a5875706302673dd84ce
andrehsvictor <andrehsvictor@gmail.com>
2025-06-19 16:08:45 -0300

    feat: enhance ReviewRepository with additional query methods for user and card reviews

--- src/main/java/andrehsvictor/memorix/review/ReviewRepository.java
+++ src/main/java/andrehsvictor/memorix/review/ReviewRepository.java
@@ -1,9 +1,22 @@
 package andrehsvictor.memorix.review;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ReviewRepository extends MongoRepository<Review, UUID> {
+
+    Page<Review> findAllByUserId(UUID userId, Pageable pageable);
+
+    Page<Review> findAllByCardIdAndUserId(UUID cardId, UUID userId, Pageable pageable);
+
+    Optional<Review> findByIdAndUserId(UUID id, UUID userId);
+
+    void deleteByUserId(UUID userId);
+
+    void deleteByCardId(UUID cardId);
 
 }
