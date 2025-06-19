abe409f31c81f5b89bcea185424d1b4084059342
andrehsvictor <andrehsvictor@gmail.com>
2025-06-19 16:29:37 -0300

    feat: add additional RabbitMQ queues for email actions and card management

--- src/main/java/andrehsvictor/memorix/common/rabbitmq/RabbitMqConfig.java
+++ src/main/java/andrehsvictor/memorix/common/rabbitmq/RabbitMqConfig.java
@@ -1,5 +1,6 @@
 package andrehsvictor.memorix.common.rabbitmq;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,56 @@ public class RabbitMqConfig {
         RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
         rabbitAdmin.setAutoStartup(true);
         return rabbitAdmin;
+    }
+
+    @Bean
+    Queue emailActionsVerifyEmailQueue() {
+        return new Queue("email-actions.v1.verify-email", true);
+    }
+
+    @Bean
+    Queue emailActionsResetPasswordQueue() {
+        return new Queue("email-actions.v1.reset-password", true);
+    }
+
+    @Bean
+    Queue emailActionsChangeEmailQueue() {
+        return new Queue("email-actions.v1.change-email", true);
+    }
+
+    @Bean
+    Queue cardsDeleteQueue() {
+        return new Queue("cards.v1.delete", true);
+    }
+
+    @Bean
+    Queue cardsDeleteAllByUserIdQueue() {
+        return new Queue("cards.v1.deleteAllByUserId", true);
+    }
+
+    @Bean
+    Queue decksDeleteQueue() {
+        return new Queue("decks.v1.delete", true);
+    }
+
+    @Bean
+    Queue usersDeleteQueue() {
+        return new Queue("users.v1.delete", true);
+    }
+
+    @Bean
+    Queue reviewsDeleteAllByCardIdQueue() {
+        return new Queue("reviews.v1.deleteAllByCardId", true);
+    }
+
+    @Bean
+    Queue minioDeleteUrlQueue() {
+        return new Queue("minio.v1.delete.url", true);
+    }
+
+    @Bean
+    Queue minioDeleteMetadataQueue() {
+        return new Queue("minio.v1.delete.metadata", true);
     }
 
 }
