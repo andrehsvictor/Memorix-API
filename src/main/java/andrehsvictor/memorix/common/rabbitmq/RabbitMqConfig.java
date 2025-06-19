a73e43a85abc7c13b1ce091309b838b6347b829c
andrehsvictor <andrehsvictor@gmail.com>
2025-06-19 08:25:13 -0300

    feat: add @Bean annotation to rabbitAdmin method in RabbitMqConfig

--- src/main/java/andrehsvictor/memorix/common/rabbitmq/RabbitMqConfig.java
+++ src/main/java/andrehsvictor/memorix/common/rabbitmq/RabbitMqConfig.java
@@ -2,11 +2,13 @@ package andrehsvictor.memorix.common.rabbitmq;
 
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
 
+    @Bean
     RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
         RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
         rabbitAdmin.setAutoStartup(true);
