f2db2a37c6fb45ee2ae15dddc86e55f40fc85371
andrehsvictor <andrehsvictor@gmail.com>
2025-06-19 11:10:37 -0300

    feat: add aggregation methods to CardRepository for user and deck card statistics; update CardStatsDto fields for consistency

--- src/main/java/andrehsvictor/memorix/card/CardRepository.java
+++ src/main/java/andrehsvictor/memorix/card/CardRepository.java
@@ -5,7 +5,10 @@ import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import andrehsvictor.memorix.card.dto.CardStatsDto;
 
 public interface CardRepository extends MongoRepository<Card, UUID> {
 
@@ -18,5 +21,27 @@ public interface CardRepository extends MongoRepository<Card, UUID> {
     Long countByUserIdAndDueBefore(UUID userId, LocalDateTime due);
 
     Long countByDeckIdAndDueBefore(UUID deckId, LocalDateTime due);
+
+    @Aggregation(pipeline = {
+            "{ '$match': { 'userId': ?0 } }",
+            "{ '$group': { '_id': null, " +
+                    "'total': { '$sum': 1 }, " +
+                    "'due': { '$sum': { '$cond': [{ '$lte': ['$due', new Date()] }, 1, 0] } }, " +
+                    "'newCards': { '$sum': { '$cond': [{ '$eq': ['$interval', 0] }, 1, 0] } }, " +
+                    "'learning': { '$sum': { '$cond': [{ '$lt': ['$interval', 21] }, 1, 0] } }, " +
+                    "'reviewed': { '$sum': { '$cond': [{ '$gt': ['$interval', 20] }, 1, 0] } } } }"
+    })
+    CardStatsDto findCardStatsByUserId(UUID userId);
+
+    @Aggregation(pipeline = {
+            "{ '$match': { 'deckId': ?0 } }",
+            "{ '$group': { '_id': null, " +
+                    "'total': { '$sum': 1 }, " +
+                    "'due': { '$sum': { '$cond': [{ '$lte': ['$due', new Date()] }, 1, 0] } }, " +
+                    "'newCards': { '$sum': { '$cond': [{ '$eq': ['$interval', 0] }, 1, 0] } }, " +
+                    "'learning': { '$sum': { '$cond': [{ '$lt': ['$interval', 21] }, 1, 0] } }, " +
+                    "'reviewed': { '$sum': { '$cond': [{ '$gt': ['$interval', 20] }, 1, 0] } } } }"
+    })
+    CardStatsDto findCardStatsByDeckId(UUID deckId);
 
 }
--- src/main/java/andrehsvictor/memorix/card/dto/CardStatsDto.java
+++ src/main/java/andrehsvictor/memorix/card/dto/CardStatsDto.java
@@ -1,14 +1,19 @@
 package andrehsvictor.memorix.card.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class CardStatsDto {
-    private Long totalCards;
-    private Long dueCards;
+    private Long total;
+    private Long due;
+
+    @JsonProperty("new")
     private Long newCards;
-    private Long learningCards;
-    private Long reviewCards;
+
+    private Long learning;
+    private Long reviewed;
 }
