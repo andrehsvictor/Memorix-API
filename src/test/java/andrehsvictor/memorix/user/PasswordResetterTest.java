0c6fb064385a40342bf46ab1d1d7d77509560f88
andrehsvictor <andrehsvictor@gmail.com>
2025-06-20 16:27:38 -0300

    refactor: clean up import statements in EmailVerifierTest and PasswordResetterTest

--- src/test/java/andrehsvictor/memorix/user/PasswordResetterTest.java
+++ src/test/java/andrehsvictor/memorix/user/PasswordResetterTest.java
@@ -3,9 +3,8 @@ package andrehsvictor.memorix.user;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
