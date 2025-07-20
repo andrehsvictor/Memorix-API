80649936311c39a52bc709d6a516857d9a032818
andrehsvictor <andrehsvictor@gmail.com>
2025-07-20 14:29:48 -0300

    feat: Add start script for development environment setup

---
+++ start.sh
@@ -0,0 +1,20 @@
+if [ -f certs/public.pem ] && [ -f certs/private.pem ]; then
+    echo "[INFO] RSA keys found in certs/ directory."
+else
+    echo "[INFO] No RSA keys found in certs/ directory. Generating new keys..."
+    chmod +x genrsa.sh
+    ./genrsa.sh
+    if [ $? -ne 0 ]; then
+        echo "[ERROR] Failed to generate RSA keys."
+        exit 1
+    fi
+    echo "[INFO] RSA keys generated successfully."
+fi
+
+docker-compose -f docker-compose.dev.yml up
+
+# Wait 2 seconds for the services to start
+sleep 2
+
+# Run the application
+./mvnw spring-boot:run -Dspring-boot.run.profiles=dev
